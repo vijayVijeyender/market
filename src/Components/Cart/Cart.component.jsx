fcf414026c4116c72d97428fd43e250ae01888c5
vijeyender <vijayvijeyender@gmail.com>
2024-04-10 21:05:20 +0530

    Props Example Add item to cart

--- src/Components/Cart/Cart.component.jsx
+++ src/Components/Cart/Cart.component.jsx
@@ -1,10 +1,11 @@
 
 
-export function Cart(){
+export function Cart(props){
 
+    
     return(
         <>
-         <div className="container d-flex flex-row flex-grow-1 flex-shrink-1 justify-content-evenly"><input className="bg-secondary-subtle border rounded-pill border-primary-subtle shadow-lg d-flex flex-row flex-grow-1" type="search" placeholder="Search for Products, Brands and More" autoFocus="" autoComplete="on" style={{ paddingLeft:"20px", margin:"0px", marginRight:"10px", marginLeft:"0px", paddingTop:"0px", paddingRight:"0px", }} /><button className="btn btn-primary" type="submit">Search</button></div>
+         <div className="container d-flex flex-row flex-grow-1 flex-shrink-1 justify-content-evenly"></div>
         <div className="offcanvas offcanvas-end" tabIndex={-1} id="navbar" aria-labelledby="navbarLabel">
             <div className="offcanvas-header">
                 <h5 className="offcanvas-title" id="navbarLabel">Cart</h5><button className="btn-close" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -20,12 +21,14 @@ export function Cart(){
                             </tr>
                         </thead>
                         <tbody>
-                           
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
+                       { props.cartItems.map((items)=>(
+                        <tr key={items.name}>
+                                <td>{items.productName}</td>
+                                <td>{items.productQty}</td>
+                                <td>{items.productPrice}</td>
                             </tr>
+                       ))}     
+                            
                               
                             <tr>
                                 <td>Total:</td>
