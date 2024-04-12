d94711825d9e44f821450778561076863fb58c74
vijeyender <vijayvijeyender@gmail.com>
2024-04-12 19:45:13 +0530

    Global store changes for brand

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,14 @@
 import { createStore as createEasyPeasyStore } from "easy-peasy";
 import { HttpService } from "../services/http/http.service";
+import { BrandModel, brandModel } from "./Models/brand/brand.model";
 
 
 
 export interface AppStoreModel{
-
+brandModel: BrandModel;
 }
 export const appStoreModel: AppStoreModel ={
-   
+   brandModel : brandModel
 }
 
 function createStore(defaultValues?: any){
